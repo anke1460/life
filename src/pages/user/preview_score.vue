386fb0705aa98d3cc9b493c9afa4821f97afcc26
anke1460 <zuosjob@gmail.com>
2016-02-29 22:55:44 +0800

    评分历史记录

--- src/pages/user/preview_score.vue
+++ src/pages/user/preview_score.vue
@@ -9,7 +9,7 @@
 	   <h1 class="mui-title">评分总览</h1>
 	   <button class="mui-btn mui-btn-blue mui-btn-link mui-pull-right" @tap="rule">评分规则</button>
 	</header>
-	<div class="mui-content mui-scroll-wrapper">
+	<div class="mui-content mui-scroll-wrapper" id="refreshContainer">
 		<div class="mui-scroll">
 			<div class="graph">
 				<div id="graph">
@@ -42,11 +42,26 @@
 				    	     <span class="score-value">{{user[category.alias + "_score"]}}</span>
 		    	    	  </div>
 		    	    	</li>
-		    	    	
 	    	    </ul>
-	    	    
-	    	    
 	    	  </div>
+	    	</div>
+	    	<div id="score_bg">
+	    	  <div id="logo_bg">
+	    	  	  <img :src="user_logo"/>
+	    	  </div>
+	    	  <div id="v_bg"></div>
+	    	  <div id="score_value">4555分</div>
+	    	  <p id="score_date">知趣：2012/10/10 -- 2015/01/22</p>
+	    	</div>
+	    	<div class="record" v-for="record in recordes">
+	    	  <div class="list-bg"></div>
+	    	  <img :src="record.score_type | to_photo" />
+	    	  <p>
+	    	  	 <span>{{record.score_type}}</span>
+	    	   <span>{{record.node}}</span>
+	    	  </p>
+	    	  <span class="recored-score">{{record.score}}分</span>
+	    	  <span class="recored-date">{{record.created_at}}</span>
 	    	</div>
 		</div>
 	</div>
@@ -63,13 +78,17 @@
 				logo4: '../images/skill.png',
 				logo5: '../images/hobby.png',
 				categories: '',
+				page: 1,
+				per_page: 20,
 				user_score: {
 					travel_score: 0,
 					food_score: 0,
 					social_score: 0,
 					skill_score: 0,
 					hobby_score: 0
-				}
+				},
+				user_logo: '../images/skill.png',
+				recordes: ''
 			}
 		},
 		watch: {
@@ -87,8 +106,20 @@
 		},
 		ready: function() {
 			var self = this;
-			mui.init();
+			var self = this;
+			mui.init({
+				pullRefresh: {
+					container: "#refreshContainer",
+					up: {
+						auto: true,
+						contentrefresh: "上拉加载更多",
+						contentnomore: '已加载完',
+						callback: self.load({page: self.page})
+					}
+				}
+			});
 			mui.plusReady(function() {
+				self.user_logo = you.getStore("logo");
 				you.authenGet("/users/score", {}, function(result) {
 					self.user = result.user;
 					self.categories = result.categories;
@@ -149,6 +180,21 @@
 			})
 		},
 		methods: {
+			load: function(params) {
+				var self = this;
+				mui.plusReady(function() {
+					you.authenGet("/uses/score_recordes", params, function(result) {
+						self.recordes = result.recordes;
+						if (self.page * self.per_page > result.total_count) {
+							mui("#refreshContainer").pullRefresh().endPullupToRefresh(true);
+						} else {
+							mui("#refreshContainer").pullRefresh().endPullupToRefresh(false);
+						}
+						self.page = self.page + 1;
+						you.endLoding();
+					})
+				})
+			},
 			rule: function() {
 				mui.openWindow({
 					url: '../score_rule.html',
@@ -159,6 +205,91 @@
 	}
 </script>
 <style lang="sass">
+  #score_bg {
+  	 background-color: #f7f7f7;
+  	 height: 95px;
+  	 position: relative;
+  }
+  
+  #logo_bg {
+  	  width: 60px;
+    height: 60px;
+    position: absolute;
+    left: 12px;
+    top: 18px;
+    background-color: #ddd;
+    border-radius: 50%;
+    img {
+	    width: 45px;
+	    height: 45px;
+	    border-radius: 50%;
+	    position: absolute;
+	    left: 8px;
+	    top: 7px;
+    }
+  }
+  #score_value {
+  	  position: absolute;
+    left: 84px;
+    top: 29px;
+    color: #333;
+    font-size: 25px;
+  }
+  
+  #score_date {
+   	position: absolute;
+    left: 84px;
+    top: 60px;
+    font-size: 12px;
+    color: #999;
+  }
+  
+  #v_bg {
+  	  height: 18px;
+    width: 10px;
+    position: absolute;
+    top: 78px;
+    bottom: 0px;
+    left: 37px;
+    background: #ddd;
+  }
+  
+  .record {
+  	  height: 55px;
+    border-top: 1px solid #ddd;
+    position: relative;
+    .list-bg {
+    	  width: 10px;
+	    height: 100%;
+	    position: absolute;
+	    left: 37px;
+	    background-color: #ddd;
+    }
+    img {
+	    	width: 36px;
+	    border-radius: 50%;
+	    position: absolute;
+	    left: 24px;
+	    top: 10px;
+    }
+    p {
+    	  position: absolute;
+	    top: 18px;
+	    left: 84px;
+    }
+    .recored-score {
+    	  position: absolute;
+	    right: 10px;
+	    top: 10px;
+	    color: #333;
+    }
+    .recored-date {
+    	  position: absolute;
+	    right: 10px;
+	    bottom: 2px;
+	    font-size: 12px;
+    }
+  }
 	body {
 		height: 100%;
 	}
